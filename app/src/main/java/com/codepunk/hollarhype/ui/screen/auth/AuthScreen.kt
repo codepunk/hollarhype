512a1e965a45cce0819a66085fad34d82e1752aa
codepunk <codepunk@codepunk.com>
2024-09-17 12:12:41 -0400

    Cleanup

--- app/src/main/java/com/codepunk/hollarhype/ui/screen/auth/AuthScreen.kt
+++ app/src/main/java/com/codepunk/hollarhype/ui/screen/auth/AuthScreen.kt
@@ -1,14 +1,10 @@
 package com.codepunk.hollarhype.ui.screen.auth
 
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.codepunk.hollarhype.ui.component.HHTopAppBar
 import com.codepunk.hollarhype.ui.theme.HollarhypeTheme
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 
 @Composable
 fun AuthScreen(
@@ -18,9 +14,11 @@ fun AuthScreen(
 ) {
     Scaffold(
         modifier = modifier,
+        /*
         topBar = {
             HHTopAppBar()
         }
+         */
     ) { innerPadding ->
         AuthNavigation(
             paddingValues = innerPadding,
