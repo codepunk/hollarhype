6493685b17ca1c7e5a3bd596d6b5ccf56dacc6f3
codepunk <codepunk@codepunk.com>
2024-10-03 17:23:59 -0400

    Added a Palette file for material palettes related to Hollarhype's primary colors

---
+++ app/src/main/java/com/codepunk/hollarhype/ui/theme/Palette.kt
@@ -0,0 +1,137 @@
+package com.codepunk.hollarhype.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+enum class Tone {
+    TONE_0,
+    TONE_5,
+    TONE_10,
+    TONE_15,
+    TONE_20,
+    TONE_25,
+    TONE_30,
+    TONE_35,
+    TONE_40,
+    TONE_50,
+    TONE_60,
+    TONE_70,
+    TONE_80,
+    TONE_90,
+    TONE_95,
+    TONE_98,
+    TONE_99,
+    TONE_100
+}
+
+val hhGreen = Color(0xFFB4F946)
+val hhBlue = Color(0xFF3BA4FF)
+val hhOrange = Color(0xFFFFB155)
+val hhMagenta = Color(0xFFFF3BFB)
+val hhGrayscaleLight = Color(0xFFFFFFFF)
+val hhGrayscaleMedium = Color(0xFFF5F5F5)
+val hhGrayscaleDark = Color(0xFF151925)
+
+val hhGreenPalette = mapOf(
+    Tone.TONE_0 to Color(0xFF000000),
+    Tone.TONE_5 to Color(0xFF0A1400),
+    Tone.TONE_10 to Color(0xFF121F00),
+    Tone.TONE_15 to Color(0xFF192B00),
+    Tone.TONE_20 to Color(0xFF223600),
+    Tone.TONE_25 to Color(0xFF2A4200),
+    Tone.TONE_30 to Color(0xFF334F00),
+    Tone.TONE_35 to Color(0xFF3B5B00),
+    Tone.TONE_40 to Color(0xFF456900),
+    Tone.TONE_50 to Color(0xFF578400),
+    Tone.TONE_60 to Color(0xFF6BA000),
+    Tone.TONE_70 to Color(0xFF7FBD00),
+    Tone.TONE_80 to Color(0xFF97DA25),
+    Tone.TONE_90 to Color(0xFFCDEE99),
+    Tone.TONE_95 to Color(0xFFDBFCA6),
+    Tone.TONE_98 to Color(0xFFF1FFD4),
+    Tone.TONE_99 to Color(0xFFF9FFE7),
+    Tone.TONE_100 to Color(0xFFFFFFFF)
+)
+
+val hhBluePalette = mapOf(
+    Tone.TONE_0 to Color(0xFF000000),
+    Tone.TONE_5 to Color(0xFF001224),
+    Tone.TONE_10 to Color(0xFF001D35),
+    Tone.TONE_15 to Color(0xFF002746),
+    Tone.TONE_20 to Color(0xFF003257),
+    Tone.TONE_25 to Color(0xFF003D69),
+    Tone.TONE_30 to Color(0xFF00497C),
+    Tone.TONE_35 to Color(0xFF00558F),
+    Tone.TONE_40 to Color(0xFF0061A2),
+    Tone.TONE_50 to Color(0xFF007BCB),
+    Tone.TONE_60 to Color(0xFF2195EF),
+    Tone.TONE_70 to Color(0xFF61B0FF),
+    Tone.TONE_80 to Color(0xFF9DCAFF),
+    Tone.TONE_90 to Color(0xFFD1E4FF),
+    Tone.TONE_95 to Color(0xFFE9F1FF),
+    Tone.TONE_98 to Color(0xFFF8F9FF),
+    Tone.TONE_99 to Color(0xFFFDFCFF),
+    Tone.TONE_100 to Color(0xFFFFFFFF)
+)
+
+val hhOrangePalette = mapOf(
+    Tone.TONE_0 to Color(0xFF000000),
+    Tone.TONE_5 to Color(0xFF1D0D00),
+    Tone.TONE_10 to Color(0xFF2B1700),
+    Tone.TONE_15 to Color(0xFF3A2000),
+    Tone.TONE_20 to Color(0xFF482900),
+    Tone.TONE_25 to Color(0xFF583300),
+    Tone.TONE_30 to Color(0xFF673D00),
+    Tone.TONE_35 to Color(0xFF774700),
+    Tone.TONE_40 to Color(0xFF885200),
+    Tone.TONE_50 to Color(0xFFA8690D),
+    Tone.TONE_60 to Color(0xFFC78229),
+    Tone.TONE_70 to Color(0xFFE69B42),
+    Tone.TONE_80 to Color(0xFFFFB868),
+    Tone.TONE_90 to Color(0xFFFFDDBB),
+    Tone.TONE_95 to Color(0xFFFFEEDF),
+    Tone.TONE_98 to Color(0xFFFFF8F4),
+    Tone.TONE_99 to Color(0xFFFFFBFF),
+    Tone.TONE_100 to Color(0xFFFFFFFF)
+)
+
+val hhMagentaPalette = mapOf(
+    Tone.TONE_0 to Color(0xFF000000),
+    Tone.TONE_5 to Color(0xFF260026),
+    Tone.TONE_10 to Color(0xFF380038),
+    Tone.TONE_15 to Color(0xFF490049),
+    Tone.TONE_20 to Color(0xFF5B005B),
+    Tone.TONE_25 to Color(0xFF6E006D),
+    Tone.TONE_30 to Color(0xFF810080),
+    Tone.TONE_35 to Color(0xFF950094),
+    Tone.TONE_40 to Color(0xFFA900A8),
+    Tone.TONE_50 to Color(0xFFD300D2),
+    Tone.TONE_60 to Color(0xFFF630F3),
+    Tone.TONE_70 to Color(0xFFFF76F5),
+    Tone.TONE_80 to Color(0xFFFFABF3),
+    Tone.TONE_90 to Color(0xFFFFD7F5),
+    Tone.TONE_95 to Color(0xFFFFEBF7),
+    Tone.TONE_98 to Color(0xFFFFF7F9),
+    Tone.TONE_99 to Color(0xFFFFFBFF),
+    Tone.TONE_100 to Color(0xFFFFFFFF)
+)
+
+val hhGrayscalePalette = mapOf(
+    Tone.TONE_0 to Color(0xFF000000),
+    Tone.TONE_5 to Color(0xFF111111),
+    Tone.TONE_10 to Color(0xFF1C1B1B),
+    Tone.TONE_15 to Color(0xFF262625),
+    Tone.TONE_20 to Color(0xFF313030),
+    Tone.TONE_25 to Color(0xFF3C3B3B),
+    Tone.TONE_30 to Color(0xFF484646),
+    Tone.TONE_35 to Color(0xFF535252),
+    Tone.TONE_40 to Color(0xFF5F5E5E),
+    Tone.TONE_50 to Color(0xFF787776),
+    Tone.TONE_60 to Color(0xFF929090),
+    Tone.TONE_70 to Color(0xFFADAAAA),
+    Tone.TONE_80 to Color(0xFFC9C6C5),
+    Tone.TONE_90 to Color(0xFFE5E2E1),
+    Tone.TONE_95 to Color(0xFFF4F0EF),
+    Tone.TONE_98 to Color(0xFFFCF8F8),
+    Tone.TONE_99 to Color(0xFFF6FEFF),
+    Tone.TONE_100 to Color(0xFFFFFFFF)
+)
